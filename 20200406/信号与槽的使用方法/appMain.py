35800350af76106958656f258221d0fd2382a9de
Aimee <961745931@qq.com>
2020-04-06 17:21:00 +0800

    信号与槽内建关联函数

--- 20200406/信号与槽的使用方法/appMain.py
+++ 20200406/信号与槽的使用方法/appMain.py
@@ -8,5 +8,3 @@ mainform = QmyDialog()  # 创建主窗体
 mainform.show()  # 显示主窗体
 sys.exit(app.exec_())
 
-
-
--- 20200406/信号与槽的使用方法/myDialog.py
+++ 20200406/信号与槽的使用方法/myDialog.py
@@ -1,6 +1,8 @@
 # # 与UI窗体类对应的业务逻辑类
 import sys
 from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtGui import QPalette
+from PyQt5.QtCore import Qt, pyqtSlot
 from Dialog import Ui_Dialog
 
 
@@ -9,6 +11,72 @@ class QmyDialog(QDialog):
         super().__init__(parent)  # 调用父类构造函数，创建QWidget窗体
         self.ui = Ui_Dialog()  # 创建UI对象
         self.ui.setupUi(self)  # 构造UI
+
+        # ///////////// 自行添加的部分 /////////////
+
+        # # 添加下划线
+        # self.ui.chkBoxUnder.clicked.connect(self.on_chkBoxUnder_clicked)
+        #
+        # # 修改编辑框中字体为斜体
+        # self.ui.chkBoxItalic.clicked.connect(self.on_chkBoxItalic_clicked)
+        #
+        # # # 修改编辑框中字体为斜体
+        # # self.ui.chkBoxItalic.toggled.connect(self.on_chkBoxItalic_toggled)
+        #
+        # # 添加加粗效果
+        # self.ui.chkBoxBold.toggled.connect(self.on_chkBoxBold_toggled)
+        #
+        # # 添加clear效果
+        # self.ui.btnClear.clicked.connect(self.on_btnClear_clicked)
+
+        # 设置颜色关联函数
+        self.ui.radioBlack.clicked.connect(self.do_setTextColor)
+        self.ui.radioRed.clicked.connect(self.do_setTextColor)
+        self.ui.radioBlue.clicked.connect(self.do_setTextColor)
+
+    # #//////////////////自定义槽函数////////////////////
+
+    # 设置文本颜色
+    def do_setTextColor(self):
+        plet = self.ui.plainTextEdit.palette()   # 获取palette
+        if(self.ui.radioBlack.isChecked()):
+            plet.setColor(QPalette.Text, Qt.black)
+        elif(self.ui.radioRed.isChecked()):
+            plet.setColor(QPalette.Text, Qt.red)
+        elif(self.ui.radioBlue.isChecked()):
+            plet.setColor(QPalette.Text, Qt.blue)
+        self.ui.plainTextEdit.setPalette(plet)
+
+    # 将编辑框里面的文字添加下划线
+    def on_chkBoxUnder_clicked(self):
+        checked = self.ui.chkBoxUnder.isChecked()  # 读取勾选状态
+        font = self.ui.plainTextEdit.font()
+        font.setUnderline(checked)
+        self.ui.plainTextEdit.setFont(font)
+
+    # 将编辑框里面的文字变为斜体
+    def on_chkBoxItalic_clicked(self):
+        checked = self.ui.chkBoxItalic.isChecked()
+        font = self.ui.plainTextEdit.font()
+        font.setItalic(checked)
+        self.ui.plainTextEdit.setFont(font)
+
+    # # 将编辑框里面的文字变为斜体
+    # def on_chkBoxItalic_toggled(self, checked):
+    #     # checked = self.ui.chkBoxItalic.isChecked()
+    #     font = self.ui.plainTextEdit.font()
+    #     font.setItalic(checked)
+    #     self.ui.plainTextEdit.setFont(font)
+
+    # 将编辑框里面的内容加粗
+    def on_chkBoxBold_toggled(self, checked):
+        font = self.ui.plainTextEdit.font()
+        font.setBold(checked)  # 参数checked表示勾选状态
+        self.ui.plainTextEdit.setFont(font)
+
+    # 清除编辑框里面的内容函数
+    def on_btnClear_clicked(self):
+        self.ui.plainTextEdit.clear()
 
 
 if __name__ == "__main__":
